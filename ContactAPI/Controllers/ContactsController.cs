d050881859db59cca972ad6bb26dd07d919003d6
EbroDore <brodybond13@gmail.com>
2023-03-15 22:08:19 +1100

    Added AddContact method created AddContactRequest for API connection

--- ContactAPI/Controllers/ContactsController.cs
+++ ContactAPI/Controllers/ContactsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactAPI.Data;
+using ContactAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactAPI.Controllers
 {
@@ -6,10 +9,36 @@ namespace ContactAPI.Controllers
     [Route("api/Contacts")]
     public class ContactsController : Controller
     {
-
-        public IActionResult Index()
+        private readonly ContactAPIDbContext _dbContext;
+        public ContactsController(ContactAPIDbContext dbContext)
         {
-            return View();
+            this._dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await _dbContext.Contacts.ToListAsync());
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                PhoneNumber = addContactRequest.PhoneNumber
+            };
+            await _dbContext.Contacts.AddAsync(contact);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(contact);
         }
     }
 }
